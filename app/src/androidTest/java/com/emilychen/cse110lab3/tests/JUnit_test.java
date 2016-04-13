23f3dff7b76d964d2731823b826feca585801fe4
e3chen <emilychen101@gmail.com>
2016-04-13 11:23:45 -0700

    first commit

---
+++ app/src/androidTest/java/com/emilychen/cse110lab3/tests/JUnit_test.java
@@ -0,0 +1,27 @@
+package com.emilychen.cse110lab3.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.emilychen.cse110lab3.MainActivity;
+
+/**
+ * Created by emilychen on 4/13/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+    public JUnit_test() {
+
+        super(MainActivity.class);
+    }
+
+    public void test_Sum() {
+        mainActivity = getActivity();
+
+        int one = 15;
+        int two = 30;
+
+        int three = mainActivity.sum(one, two);
+        assertEquals(45, three);
+    }
+}
---
+++ app/src/main/java/com/emilychen/cse110lab3/MainActivity.java
@@ -0,0 +1,73 @@
+package com.emilychen.cse110lab3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button butt = (Button) findViewById(R.id.button);
+        butt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText text1 = (EditText) findViewById(R.id.editText);
+                EditText text2 = (EditText) findViewById(R.id.editText2);
+                int num1 = Integer.parseInt(text1.getText().toString());
+                int num2 = Integer.parseInt(text2.getText().toString());
+                int total = sum(num1, num2);
+
+                TextView text3 = (TextView) findViewById(R.id.textView);
+                text3.setText(Integer.toString(total));
+            }
+        });
+    }
+
+    public int sum(int one, int two){
+        return one + two;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
